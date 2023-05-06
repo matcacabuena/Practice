d8d2a55537b08f7f2fb2cbc1c889f50b6999666d
Carolina Ferreira <ninaferreira2409@gmail.com>
2023-05-06 05:39:51 -0300

    tela de listagem

--- TestePratico/src/contexts/auth.jsx
+++ TestePratico/src/contexts/auth.jsx
@@ -6,51 +6,9 @@ export const AuthContext = createContext({})
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState();
   const [signed, setSigned] = useState(false);
-
-
-  /*useEffect(() => {
-    const userToken = localStorage.getItem("token");
-    const usersStorage = localStorage.getItem("users_bd");
-    console.log(userToken.email)
-
-    if (userToken && usersStorage) {
-      const hasUser = JSON.parse(usersStorage)?.filter(
-        (user) => user.email === JSON.parse(userToken).email
-      );
-
-      if (hasUser) setUser(hasUser[0]);
-    }
-  }, []);*/
-
-
-
-  /*const login = (email, password) => {
-    
-    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
-
-    const hasUser = usersStorage?.filter((user) => user.email === email);
-
-    if (hasUser?.length) {
-      if (hasUser[0].email === email && hasUser[0].password === password) {
-        //localStorage.setItem("token", localStorage.getItem("token"));
-        setUser({ email, password });
-        return;
-      } else {
-        return "E-mail ou senha incorretos";
-      }
-    } else {
-      console.log(email)
-      console.log(password)
-      console.log(localStorage.getItem("token"))
-      return "Usuário não cadastrado";
-    }
-  };*/
+  const [sellers, setSellers] = useState([])
 
   const login = async (email, password) => {
-    //const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
-
-      //const newUser = [{ email, password }];
-      //localStorage.setItem("users_bd", JSON.stringify(newUser));
 
       const rs = await fetch("https://m2devadmin.softkuka.com.br/api/Login", {
         method: "POST",
@@ -72,22 +30,22 @@ export const AuthProvider = ({ children }) => {
       return !rs?.status;
   };
 
-  function getData() {
+  const sellerList = async () => {
     const token = localStorage.getItem("token")
-    fetch('https://m2devadmin.softkuka.com.br/api/Vendedor', {
+    const rs = await fetch('https://m2devadmin.softkuka.com.br/api/Vendedor', {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` }
     })
       .then(response => response.json())
-      .then(console.log)
+      // .then(response => setSellers(response))
       .catch(mistake => console.log('Error: ', mistake))
-
-
+      
+      return rs;
   }
 
   return (
     <AuthContext.Provider
-    value={{ user, signed, login}}
+    value={{ user, signed, login, sellerList}}
     >
       {children}
     </AuthContext.Provider>
--- TestePratico/src/pages/Home/index.jsx
+++ TestePratico/src/pages/Home/index.jsx
@@ -1,8 +1,59 @@
+import { Box, Button, Flex } from "@chakra-ui/react"
+import useAuth from "../../hooks/useAuth";
+import { useEffect, useState } from "react";
+import "./styles.css"
 
-function List() {
+const List = () => {
+  const { sellerList } = useAuth();
+  const [sellers, setSellers] = useState([]);
+  const showSellers = async () => {
+    const res = await sellerList()
+    res.forEach(element => {
+      console.log(element.nome)
+    });
+    setSellers(res)
+    //res = array de vendedores
+  }
+  useEffect(() => {
+    showSellers()
+  }, [])
+
   return (
-    <div>List</div>
+    <div className="content">
+      <header className="header">
+        <h1>Sellers List</h1>
+        <input className="search" type="search" placeholder="Pesquise um vendedor" />
+        <a href="#">sair</a>
+      </header>
+      <div className="sellerList">
+        {sellers.map(({ id, nome, cnpj, idEmpresa, criadoEm }) =>
+          <div className="seller">
+            <ol hidden="">
+              <li><strong>ID:</strong> {id}</li>
+              <li><strong>Nome:</strong> {nome}</li>
+              <li><strong>CNPJ:</strong> {cnpj}</li>
+              <li><strong>ID da Empresa:</strong> {idEmpresa}</li>
+              <li><strong>Criado em:</strong> {criadoEm}</li>
+            </ol>
+          </div>)}
+        <Button colorScheme='teal' onClick={getData} className="get">Checar o GET</Button>
+      </div>
+    </div>
   )
+}
+
+async function getData() {
+  const token = localStorage.getItem("token")
+  const rs = await fetch('https://m2devadmin.softkuka.com.br/api/Vendedor', {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` }
+  })
+    .then(response => response.json())
+    .then(console.log)
+    .catch(mistake => console.log('Error: ', mistake))
+
+  return rs;
+
 }
 
 export default List
