4b091ddfb6031bc3c9164791582f1173b607a36b
Carolina Ferreira <ninaferreira2409@gmail.com>
2023-05-06 05:39:51 -0300

    tela de listagem

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -14,4 +14,5 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
+        .catch()
 }
