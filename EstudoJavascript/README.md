4cea88fc815897f4f2710accbf0e2afff7a7a097
Mateus Cacabuena <mateuscacabuena@gmail.com>
2023-07-13 20:23:22 -0300

    fix: javascript study organization

---
+++ EstudoJavascript/README.md
@@ -0,0 +1,3 @@
+#   Pokedex
+
+Com as anotações feitas em aula, pude praticar a implementação de uma pokedex da geração de Kanto, implementada com JavaScript, HTML e CSS.
--- EstudoJavascript/assets/js/main.js
+++ EstudoJavascript/assets/js/main.js
@@ -5,18 +5,18 @@ function convertPokemonTypesToLi(pokemonTypes) {
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
-                <span class="number">#00${pokemon.order}</span>
-                <span class="name">${pokemon.name}</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
 
-                <div class="detail">
-                    <ol class="types">
-                        ${convertPokemonTypesToLi(pokemon.types).join('')}
-                    </ol>
-                    <img src="${pokemon.sprites.other.dream_world.front_default}" 
-                        alt="${pokemon.name}">
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
 
-                </div>
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
         </li>
     `
 }
