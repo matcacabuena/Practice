34b2b3e91dc1013792607878f1de32a5b852f3a5
Mateus Cacabuena <mateuscacabuena@gmail.com>
2023-08-27 12:19:21 -0300

    add: estudo go (lang)

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main //pacote principal
+
+import "fmt"
+
+func main() { //função principal
+	/*
+		1. "go build hello.go" para criar o executável
+	  	2. "go run hello.go" para executar
+	*/
+	fmt.Println("Hello World!")
+
+}
