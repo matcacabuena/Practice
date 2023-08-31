6655daf10ad233070a4fc25d635839ecd6d128df
MateusNotebook <mateuscacabuena@gmail.com>
2023-08-31 20:35:00 -0300

    add: desafio 3 completo

--- Desafio3/PingPong.go
+++ Desafio3/PingPong.go
@@ -6,32 +6,31 @@ import (
 )
 
 func main() {
-
+	fmt.Println("Desafio 3 - Ping Pong")
 	c1 := make(chan string)
 	c2 := make(chan string)
-
+	
 	go func() {
 		for {
+			time.Sleep(1 * time.Second)
 			c1 <- "Ping"
-			time.Sleep(1 * time.Second)
 		}
 	}()
 
 	go func() {
 		for {
-			c2 <- "Pong"
 			time.Sleep(1 * time.Second)
+			<-c1 // Espera o Ping para o Pong
+			c2 <- "Pong"
 		}
 	}()
 
-	go func() {
-		for i:=0; i<10; i++ {
-			select {
-			case msg1 := <-c1:
-				fmt.Println(msg1)
-			case msg2 := <-c2:
-				fmt.Println(msg2)
-			}
+	for i := 0; i < 10; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println(msg1)
+		case msg2 := <-c2:
+			fmt.Println(msg2)
 		}
-	}()
+	}
 }
